323b155bb073d5b2bac4105a35ac350d130ebc90
capricornus44 <antonzavalniuk@gmail.com>
2021-04-07 16:57:39 +0200

    create initial project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import MoviesPage from './pages/MoviesPage';
+import MovieDetailsPage from './pages/MovieDetailsPage';
+import Cast from './pages/Cast';
+import Reviews from './pages/Reviews';
+
 import './App.module.scss';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => (
+  <>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Route exact path="/" component={HomePage}></Route>
+    <Route path="/movies" component={MoviesPage}></Route>
+    <Route path="/movies/:movieId" component={MovieDetailsPage}></Route>
+    <Route path="/movies/:movieId/cast" component={Cast}></Route>
+    <Route path="/movies/:movieId/reviews" component={Reviews}></Route>
+  </>
+);
 
 export default App;
---
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -0,0 +1,5 @@
+const MovieDetailsPage = () => {
+  return <h1>Movie details page</h1>;
+};
+
+export default MovieDetailsPage;
