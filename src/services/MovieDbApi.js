839d83fbdf375274963c2cf8763c06c307bfa974
capricornus44 <antonzavalniuk@gmail.com>
2021-04-08 18:02:04 +0200

    Create MovieDbApi.js with all requests to the server

---
+++ src/services/MovieDbApi.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const API_KEY = '989c90c59500ad26e3fa4e26d53d2bd3';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const fetchTrendingMovies = () => {
+  return axios
+    .get(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`)
+    .then(response => response.data.results);
+};
+
+const fetchMovieDetails = movieId => {
+  return axios
+    .get(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}&language=en-US`)
+    .then(response => response.data);
+};
+
+const fetchCast = movieId => {
+  return axios
+    .get(`${BASE_URL}/movie/${movieId}/credits?api_key=${API_KEY}`)
+    .then(response => response.data.cast);
+};
+
+const fetchReviews = movieId => {
+  return axios
+    .get(
+      `${BASE_URL}/movie/${movieId}/reviews?api_key=${API_KEY}&language=en-US&page=1`,
+    )
+    .then(response => response.data.results);
+};
+
+const fetchMoviesUponRequest = query => {
+  return axios
+    .get(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`)
+    .then(response => response.data.results);
+};
+
+export default {
+  fetchTrendingMovies,
+  fetchMovieDetails,
+  fetchCast,
+  fetchReviews,
+  fetchMoviesUponRequest,
+};
