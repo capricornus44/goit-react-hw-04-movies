28c4be94115005d2c8e90f6dfeb777293f3a9ec1
capricornus44 <antonzavalniuk@gmail.com>
2021-04-08 18:08:13 +0200

    Move component 'Reviews' to component's folder

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,7 @@
+const Reviews = () => {
+  return <h1>Reviews</h1>;
+};
+
+export default Reviews;
+
+//https://api.themoviedb.org/3/movie/{movieId}/reviews?api_key=989c90c59500ad26e3fa4e26d53d2bd3&language=en-US&page=1
